a35929c061ca8c012cf0e09cc03dfdd1d9d58411
yuhqns <144075745+yuhqns@users.noreply.github.com>
2024-05-23 13:13:39 +0800

    Add files via upload

--- EX5/main.cpp
+++ EX5/main.cpp
@@ -1,1 +1,53 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
 
+using namespace std;
+
+class myString {
+    private:
+        string m_str;
+    public:
+        myString(string s) {
+            m_str = s;
+        }
+};
+
+class ReadClass {
+    private:
+        int m_classCount = 0;
+        string classNames[2^5];
+    public:
+        ReadClass() {}
+        void showClass() {
+            ifstream in;
+            string line;
+            in.open("main.cpp");
+
+            while (getline(in, line)) {
+                if (line.find("class") == 0){
+                    classNames[m_classCount++] = line;
+                }
+            }
+            in.close();
+            
+            if (m_classCount) {
+                cout << m_classCount << " class" << " in main.cpp" << endl;
+                for (int i=0; i<m_classCount; i++) {
+                    stringstream str(classNames[i]);
+                    for (int j=0; j<m_classCount; j++) {
+                        getline(str, line, ' ');
+                        cout << line << " ";
+                    }
+                    cout << endl;
+                }
+            }
+        }
+};
+
+int main() {
+    ReadClass rfile;
+    rfile.showClass();
+    return 0;
+}
